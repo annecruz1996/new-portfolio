e3aac9a60363ba424c19e66b3593eb02e4811023
annecruz1996 <annecruz1996@gmail.com>
2018-07-07 17:29:47 -0500

    new portfolio so i can follow the instructions

---
+++ index.js
@@ -0,0 +1,14 @@
+// new Vue ({
+//     el: '#app',
+//     data: {
+//         text: 'hello world!'
+//     }
+// });
+
+
+new Vue ({
+    el: '#one_',
+    data: {
+        title: 'Code Delivered With Heart'
+    }
+});
